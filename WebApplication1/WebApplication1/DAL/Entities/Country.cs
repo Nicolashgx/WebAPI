190f35484528d6be4de68affece4fae0cb75ee40
ACER <ACER@DESKTOP-PH1R48V>
2025-06-05 17:27:06 -0500

    ICountry Service Created

---
+++ WebApplication1/WebApplication1/DAL/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DAL.Entities
+{
+    public class Country : AuditBase
+    {
+        [Display(Name = "País")] //identificar mas facil
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener un Maximo de {1} caracteres") ] // limitar el numero de caracteres
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio
+        public string Name { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Domain/Interfaces/ICountryService.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication1.DAL.Entities;
+
+namespace WebApplication1.Domain.Interfaces
+{
+    public interface ICountryService
+    {
+        Task<IEnumerable<Country>> GetCountriesAsync();
+
+        Task<Country> CreateCountryAsync(Country country);
+
+        Task<Country> GetCountryById(Guid id);
+
+        Task<Country> EditCountryAsync(Country country);
+
+        Task<Country> DeleteCountryAsync(Guid id);
+    }
+}
