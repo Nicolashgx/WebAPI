95895d8c3ed7c799fe104cb5e8a304a16224e9f7
ACER <ACER@DESKTOP-PH1R48V>
2025-06-05 22:32:44 -0500

    act

---
+++ WebApplication1/WebApplication1/Controllers/CountriesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DAL.Entities;
+using WebApplication1.Domain.Interfaces;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class CountriesController : Controller
+    {
+        private readonly ICountryService _countryService;
+
+        public CountriesController(ICountryService countryService)
+        {
+            _countryService = countryService;
+
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetAll")]
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountriesAsync { 
+        
+        
+        
+        }
+        
+
+
+    }
+}
